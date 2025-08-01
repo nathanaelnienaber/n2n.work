bbf67fc1a49472101cc874c3a5c580d45a7d1591
nathanael nienaber <nn@tmrw.it>
2025-08-01 08:47:56 -0700

    added email to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      &copy; 2025 TMRW Consulting Inc. All rights reserved.
+      &copy; 2025 TMRW Consulting Inc. All rights reserved. Email address: hello@tmrw.it 
     </footer>
   );
 }
