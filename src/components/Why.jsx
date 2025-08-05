de697a44b7481a1e4da681ba6284f29d17b1d52e
Nathanael Nienaber <142456937+nathanaelnienaber@users.noreply.github.com>
2025-08-05 14:39:13 -0700

    feat: add bullet list utility

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -4,13 +4,13 @@ export default function Why() {
       <div className="container section-content">
         <h2>Why Work With Me in the Age of AI.</h2>
         <p>AI can suggest tools.</p>
-        <p>-> I help you choose the right ones — and actually implement them with your team.</p>
+        <p>&rarr; I help you choose the right ones — and actually implement them with your team.</p>
         <br></br>
         <p>Consultants love strategy decks.</p>
-        <p>-> I roll up my sleeves and help you get it done.</p>
+        <p>&rarr; I roll up my sleeves and help you get it done.</p>
         <br></br>
         <p>You don’t need a expert with a bunch of buzzwords.</p>
-        <p>-> I understand how real businesses run — and what it takes to modernize them respectfully.</p>
+        <p>&rarr; I understand how real businesses run — and what it takes to modernize them respectfully.</p>
         <br></br>
       </div>
     </section>
