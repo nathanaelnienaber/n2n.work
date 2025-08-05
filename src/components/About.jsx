9d5f81805068f311b6d06e9bdd4ac340cd31c22a
nathanael nienaber <nn@tmrw.it>
2025-08-05 14:28:15 -0700

    updated hero text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,13 @@ export default function About() {
     <section id="about">
       <div className="container section-content">
         <h2>You’ve Built a Business That Lasts. Now Let’s Make It Easy to Run.</h2>
-        <p>Your business has stood the test of time — but behind the scenes, things might feel clunky:
-          <ul className="list-disc pl-6">
+        <p>Your business has stood the test of time — but behind the scenes, things might feel clunky:</p>  
+        <ul className="list-disc pl-6">
             <li>Manual processes</li>
             <li>Overlapping tools</li>
             <li>Teams working around systems instead of with them</li>
             <li>Your tech clutter worrys you</li>
           </ul>
-        </p>
         <p>You’re not looking to “blow it all up.” You just want things to work better.
 That’s where I come in.</p>
       </div>
