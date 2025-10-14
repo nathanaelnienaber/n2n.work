3510c580d8d3d3b79b026a225e3596f76c0ad16b
Nathanael Nienaber <142456937+nathanaelnienaber@users.noreply.github.com>
2025-10-14 15:41:39 -0700

    Update Footer.jsx
    changed to TMRW LLC

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      &copy; 2025 TMRW Consulting Inc. All rights reserved. Email address: hello@n2n.work 
+      &copy; 2025 TMRW LLC (parent LLC). All rights reserved. Email address: hello@tmrw.it 
     </footer>
   );
 }
